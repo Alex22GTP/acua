c7d1289eaa0b9fbeee6d1ce6304124e123629f0a
thaliaGA <alondra.garcia.21s@utzmg.edu.mx>
2025-03-03 21:36:53 -0600

    interfaz escenario
    interfaz escenario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
+import EscenarioIncendios1 from './views/esc1-incendios';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/escIncendio1" element={<EscenarioIncendios1 />} />
       </Routes>
     </Router>
   );
